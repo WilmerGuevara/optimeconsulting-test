4ddb77ada9a909bdb3d342262e098371136ff240
Wilmer Guevara <wilmer.guevara@railskarate.com>
2017-12-17 21:16:02 -0500

    bootstrap was added into web/public, product's CRUD was improved using bootstrap, pagination was improved using bootstrap

--- src/OptimeBundle/Form/ProductType.php
+++ src/OptimeBundle/Form/ProductType.php
@@ -5,6 +5,8 @@ namespace OptimeBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class ProductType extends AbstractType
 {
@@ -13,7 +15,8 @@ class ProductType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('code')->add('name')->add('description')->add('brand')->add('price')->add('category');
+        $builder
+        ->add('code',TextType::class)->add('name')->add('description')->add('brand')->add('price')->add('category');
     }
     
     /**
