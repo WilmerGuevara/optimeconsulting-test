d67d186bb9548260aa8880e3b2077dbfb0a5ba15
Wilmer Guevara <wilmer.guevara@railskarate.com>
2017-12-18 09:04:46 -0500

    Delete category or product confirm alert before sending form

--- web/public/js/utils.js
+++ web/public/js/utils.js
@@ -1,8 +1,19 @@
-function confirmOption(message) {
-	if(window.confirm(message)){
-        alert('Eliminado');
-    } else {
-        return false;
-    }
-}
+$(document).ready(function() {
+    $("#product_edit").click(function(event) {
+        if( !confirm('¿Seguro desea eliminar el producto?') ) 
+            event.preventDefault();
+    });
+    $("#product_show").click(function(event) {
+        if( !confirm('¿Seguro desea eliminar el producto?') ) 
+            event.preventDefault();
+    });
+    $("#category_edit").click(function(event) {
+        if( !confirm('¿Seguro desea eliminar la categoría?') ) 
+            event.preventDefault();
+    });
+    $("#category_show").click(function(event) {
+        if( !confirm('¿Seguro desea eliminar la categoría?') ) 
+            event.preventDefault();
+    });
+});
     
